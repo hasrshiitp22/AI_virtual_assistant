b88da984d7928f4f95d003a2bbd368f6493b685c
harsh <hr635332@gmail.com>
2026-07-04 21:17:34 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -5,8 +5,8 @@ export const userDataContext = createContext()
 
 function UserContext({ children }) {
 
-  const serverUrl = "http://localhost:8000"
-
+  const serverUrl = "https://ai-virtual-assistant-r1yt.onrender.com";
+  
   const [userData, setUserData] = useState(null)
   const [frontendImage, setfrontendImage] = useState(null)
   const [backendImage, setbackendImage] = useState(null)
